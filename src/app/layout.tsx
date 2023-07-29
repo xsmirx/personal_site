5397b683946fcdb9098af8bdc5ffd6f605f8a97e
Ivan Smirnov <75281975+xsmirx@users.noreply.github.com>
2023-07-29 04:46:19 +0400

    add colors and font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,21 @@
 import './globals.css'
 import type {Metadata} from 'next'
+import {Outfit} from 'next/font/google'
+
+const outfit = Outfit({
+  display: 'swap',
+  subsets: ['latin'],
+  variable: '--font-outfit',
+})
 
 export const metadata: Metadata = {
   title: 'Ivan Smirnov',
-  description: 'Personal site of Ivan Smirnov (xsmirx) frontemd developer',
+  description: 'Personal website of front-end developer Ivan Smirnov (xsmirx)',
 }
 
 export default function RootLayout({children}: {children: React.ReactNode}) {
   return (
-    <html lang="en">
+    <html lang="en" className={outfit.variable}>
       <body>{children}</body>
     </html>
   )
